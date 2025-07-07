1bd4470137cc9c6f5334c8f3ad75037f9419b6ef
Grecea Mihai-Alexandru <greceaalexandru@yahoo.com>
2025-07-08 01:47:24 +0300

    dark mode cu save pe localStorage si sincronizat intre pagini

--- java.js
+++ java.js
@@ -1,15 +1,30 @@
-function seteazaTemaDupaOra() {
-    const oraCurenta = new Date().getHours();
-    const corpPagina = document.body;
+const toggle = document.getElementById('toggleDark');
+const body = document.body;
 
-    if (oraCurenta >= 6 && oraCurenta < 20) {
-        corpPagina.classList.add('light');
-        corpPagina.classList.remove('dark')
-    } else {
-        corpPagina.classList.add('dark');
-        corpPagina.classList.remove('light')        
-    }
-}
+window.addEventListener('DOMContentLoaded', () => {
+  const savedTheme = localStorage.getItem('theme');
 
-window.addEventListener('DOMContentLoaded', seteazaTemaDupaOra);
-setInterval(seteazaTemaDupaOra, 60 * 60 * 1000);
+  if (savedTheme === 'dark') {
+    body.classList.add('dark-mode');
+    toggle.classList.remove('bi-brightness-high-fill');
+    toggle.classList.add('bi-moon');
+  } else {
+    body.classList.remove('dark-mode');
+    toggle.classList.remove('bi-moon');
+    toggle.classList.add('bi-brightness-high-fill');
+  }
+});
+
+toggle.addEventListener('click', function () {
+  const isDark = body.classList.toggle('dark-mode');
+
+  if (isDark) {
+    this.classList.remove('bi-brightness-high-fill');
+    this.classList.add('bi-moon');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    this.classList.remove('bi-moon');
+    this.classList.add('bi-brightness-high-fill');
+    localStorage.setItem('theme', 'light');
+  }
+});
